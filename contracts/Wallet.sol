55da10802c73cdf680096f8d1ef99aa7893b900d
Hasan0734 <Jahidhasan2856@gmail.com>
2024-10-25 09:50:55 +0600

    add condition of send user amount

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -2,29 +2,36 @@
 pragma solidity >= 0.5.0 < 0.9.0;
 
 contract Wallet {
-    string public name = "wallet";
+ 
+    string public name = "wallet";    //read
     uint num;
+
 
     function setValue(uint _num) public {
         num = _num;
     }
-
+    //read
     function getValue() public  view  returns (uint){
         return  num;
     }
 
     function sendEthContract() public payable {}
 
+    //read
     function contractBalance() public view  returns (uint){
         return  address(this).balance;
     }
 
     function sendEthUser (address payable  _user, uint _amount) public payable   {
+            require(address(this).balance >= _amount, "Insaficient amount");
             _user.transfer(_amount);
 
     }
-
+    //read
     function accountBalance(address _address) public view  returns (uint){
         return (_address).balance;
     }
 }
+
+
+//0x3c6730ab4899d0e20ec343d7bcfa38c4e0e47680
